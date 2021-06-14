4d9b3f7e979b150e1a0fde28bf7f7b4e44fad9b6
Ivanildo Batista da Silva Junior <juniorw.12@outlook.com>
2021-06-14 02:55:19 -0300

    Add files via upload

---
+++ Uso de Software na Análise de dados Biométricos/Atividade 5/exemplos.R
@@ -0,0 +1,21 @@
+install.packages("corrgram")
+library(corrgram)
+
+install.packages("ellipse")
+library(ellipse)
+
+
+if (!requireNamespace("devtools")) install.packages("devtools")
+devtools::install_github("caijun/ggcorrplot2")
+library(ggcorrplot2)
+library(psych)
+
+
+install.packages("GGally")
+library(GGally)
+
+install.packages("correlation")
+install.packages('see')
+library(correlation)
+library(dplyr)
+library(see)
