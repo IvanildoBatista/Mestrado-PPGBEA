8cbe8905e3fd7358411a9c388c13d5471f0582ec
Ivanildo Batista da Silva Junior <juniorw.12@outlook.com>
2021-06-17 00:26:33 -0300

    Add files via upload

---
+++ Estatística Aplicada/Resolução da nona lista de exercícios/QUESTÃO 5.R
@@ -0,0 +1,5 @@
+prop.test(x = c(60, 40), n = c(400,225))
+
+#O valor p do teste é 0.4263, que é maior que o nível de
+#significância alfa = 0,05. Podemos concluir que a proporção de fumantes 
+#é significativamente igual nos dois grupos com um valor de p = 0.4263.
